8077309d578855cd6c3e2fd3935dd926bcc4e2a2
Adam Benovic <adam.benovic@intechon.sk>
2020-04-02 15:05:21 +0200

    Initial commit

---
+++ src/main/java/pis_library/PisLibraryApplication.java
@@ -0,0 +1,13 @@
+package pis_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PisLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PisLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pis_library/PisLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pis_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PisLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
