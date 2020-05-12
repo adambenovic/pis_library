567cd4b85f0c5e57392e3832e269f9e01d2dc424
Adam Benovic <adam.benovic@intechon.sk>
2020-05-12 17:05:56 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM maven:3.6.3-jdk-11-openj9 AS build
+FROM maven:3.6.3-openjdk-11 AS build
 RUN mkdir -p /workspace
 WORKDIR /workspace
 COPY pom.xml /workspace
 COPY src /workspace/src
-RUN mvn -f pom.xml clean package
+RUN mvn -f pom.xml  -Dmaven.test.skip=true clean package
 
-FROM openjdk:9
+FROM openjdk:11
 COPY --from=build /workspace/target/*.jar app.jar
 EXPOSE 8000
 ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/pis_library/endpoint/ISICValidatorEndpoint.java
+++ src/main/java/pis_library/endpoint/ISICValidatorEndpoint.java
@@ -9,6 +9,9 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import pis_library.manager.ValidationManager;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Endpoint
 public class ISICValidatorEndpoint {
     private static final String NAMESPACE_URI = "http://localhost:8000/api/ws/isicValidator";
@@ -26,9 +29,9 @@ public class ISICValidatorEndpoint {
         String isic = request.getIsicNumber();
         ValidateISICResponse response = new ValidateISICResponse();
 
-        //TODO logic to isic validation
-
-        response.setSuccess(isic.length() == 10);
+        Pattern pattern = Pattern.compile("^[a-zA-Z][0-9]{12}[a-zA-Z]$");
+        Matcher matcher = pattern.matcher(isic);
+        response.setSuccess(matcher.matches());
 
         return response;
     }
